5f28504440adbb666c626c96874bfb4c3b63e059
Serina Ko <serinako08@gmail.com>
2018-12-29 00:59:25 +1100

    fix typo

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
       <h2 className='home-title'>SERINA KO</h2>
       <img src={about} width='20' height='20' />
       <p className='home-subtitle'>
-        What matter to me is to enjoy
+        What matters to me is to enjoy
         <span className='boldText'>creating</span>,<br />
         <span className='boldText'>connecting with others</span>,<br />
-        and <span className='boldText'>getting in touch with you</span>.
+        and <span className='boldText'>getting in touch with you.</span>.
       </p>
     </div>
   );
